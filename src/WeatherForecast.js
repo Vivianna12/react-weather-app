9e202ddf9e2a8f60d9382daaa9d8b9e7217e88d1
Vivianna12 <viviannasamite12@gmail.com>
2024-03-15 17:36:08 -0400

    made my forecast work based on the user input, removed the links for temp and made my forecast show the temp in imperial

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
@@ -7,6 +7,11 @@ import axios from "axios";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.city]);
+
   function handleResponse(response) {
     console.log(response.data);
     setForecast(response.data.daily);
@@ -18,16 +23,22 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else return null;
+          })}
         </div>
       </div>
     );
   } else {
     let city = props.city;
     let apiKey = "f42atbc02aod4017c837cbfe1c70ef22";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
 
     axios.get(apiUrl).then(handleResponse);
     return null;
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,42 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState(`fahrenheit`);
-  function covertToCelsius(event) {
-    event.preventDefault();
-    setUnit("Celsius");
-  }
-
-  function covertToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  if (unit === "fahrenheit") {
-    return (
-      <div className="WeatherTemperature ">
-        <span className="weather-app-value">
-          {Math.round(props.fahrenheit)}
-        </span>
-        <span className="weather-app-unit">
-          °F |
-          <a href="/" onClick={covertToCelsius}>
-            °C
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let celsius = ((props.fahrenheit - 32) * 5) / 9;
-    return (
-      <div className="WeatherTemperature ">
-        <span className="weather-app-value">{Math.round(celsius)}</span>
-        <span className="weather-app-unit">
-          <a href="/" onClick={covertToFahrenheit}>
-            °F
-          </a>
-          | °C
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature ">
+      <span className="weather-app-value">{Math.round(props.fahrenheit)}</span>
+      <span className="weather-app-unit">°F </span>
+    </div>
+  );
 }
